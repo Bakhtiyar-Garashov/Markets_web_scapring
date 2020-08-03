d5992169f243030bb38d1f09c90e99675a2b0e79
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-03 16:15:58 +0300

    Elvi ready

--- data extraction/Elvi.py
+++ data extraction/Elvi.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import re
+import time
 from bs4 import BeautifulSoup
 
 url = "http://elvi.lv/elvi-veikali/"
@@ -27,16 +28,31 @@ def scrape_elvi(url):
     output = re.sub(r'\blink\b', '"link"', output)
     output = re.sub(r'\blat\b', '"lat"', output)
     output = re.sub(r'\blng\b', '"lng"', output)
+    output = json.loads(output)
+    for i in output:
+        additional_data = get_html_data(i['data'][0]['link'])
+        i['data'][0]['address'] = additional_data['Adrese']
+        i['data'][0]['email'] = additional_data['E-pasts']
+        i['data'][0]['phone'] = additional_data['Tālruņa numurs']
+        i['data'][0]['working_hours'] = additional_data['Darba laiks']
 
     with open("Elvi.json", 'w', encoding='utf8') as f:
-        f.write(json.loads(json.dumps(output, ensure_ascii=False, indent=4)))
-
-    with open('Elvi.json', 'r', encoding='utf8') as f:
-        data = json.loads(f.read())
-        for i in data:
-            print(i)
+        f.write(json.dumps(output, ensure_ascii=False, indent=4))
 
 
+def get_html_data(chain_url):
+    time.sleep(2)
+    response = requests.get(chain_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    info_div = soup.find('div', attrs={'class': 'info'})
+    test = dict()
+    props = info_div.select('p')
+    test[props[0].text] = props[1].text
+    test[props[2].text] = props[3].text
+    test[props[4].text] = props[5].text
+    test[props[6].text] = props[7].text
+
+    return test
 
 
 scrape_elvi(url)
