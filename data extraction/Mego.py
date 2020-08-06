a14dc6e643c4b0f90246e1604f2a3910aacc5f3d
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-06 23:13:25 +0300

    Mego store extracted

--- data extraction/Mego.py
+++ data extraction/Mego.py
@@ -18,6 +18,9 @@ def scrap_mego(url):
     raw_json = json.loads(chunk[0])
     for i in raw_json:
         i['address'] = BeautifulSoup(i['info'], 'html.parser').text.rstrip()
+        each_div = soup.find('div', attrs={'data-shop-id': i['shop_id']})
+        working_hours = each_div.select('p')[0].text.strip()
+        i['working_hours'] = working_hours
 
     with open("Mego.json", "w", encoding='utf8') as f:
         f.write(json.dumps(raw_json, ensure_ascii=False, indent=4))
