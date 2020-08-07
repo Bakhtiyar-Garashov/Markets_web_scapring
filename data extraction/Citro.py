eca5b678199160d8282703a0db1305329f9be9de
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-07 13:42:51 +0300

    Citro extracted

--- data extraction/Citro.py
+++ data extraction/Citro.py
@@ -1,7 +1,6 @@
 import json
 import requests
 import re
-import time
 from bs4 import BeautifulSoup
 
 url = "https://citro.lv/musu-veikali/"
@@ -12,9 +11,53 @@ def scrap_citro(url):
     soup = BeautifulSoup(response.content, 'html.parser')
     all_scripts = soup.find_all('script')
     all_scripts = list(all_scripts)
-    chunk = repr(all_scripts[15])
-    pattern = r"\[(.|\n)*];"
-    print(re.findall(pattern, chunk))
+    chunk = all_scripts[15]
+    test = r"\[(.|\n)*];"
+    found = re.search(test, str(chunk))
+    result = found.group(0)
+    useless = [
+        '"Veikals un kafejnīca Apsīte"',
+        '"Bodnieki"',
+        '"Ēdoles dzirnavas"',
+        '"Pārslas"',
+        '"Saktas"',
+        '"Ģipkas veikals"',
+        '"Veikals Nr.22"',
+        '"Zvirgzdu pansija"',
+        '"Spāres veikals"',
+        '"Gundegas"',
+        '"Rozes"']
+
+    output = re.sub(r'\bcoords\b', '"coords"', result)
+    output = re.sub(r'\bshopLogo\b', '"shopLogo"', output)
+    output = re.sub(r'\blat\b', '"lat"', output)
+    output = re.sub(r'\blng\b', '"lng"', output)
+    output = re.sub(r'\bcontent\b:', '"content":', output)
+    output = output.replace("'", '"')
+    output = re.sub(r'\bstyle="margin:0 0 5px 0;font-size:16px;"', "style='margin:0 0 5px 0;font-size:16px;'", output)
+    for each in useless:
+        output = output.replace(each, "'" + each + "'")
+    output = output.replace("'\"", "'")
+    output = output.replace("\"'", "'")
+    output = output.strip()
+    output = output[:len(output) - 126] + "]"
+    final = []
+    data = json.loads(output)
+
+    for i in data:
+        temp_dict = dict()
+        souplar = BeautifulSoup(i['content'], 'html.parser')
+        address = souplar.find('strong').text
+        working_hours = souplar.find_all('p')[1].text
+        temp_dict["coords"] = i['coords']
+        temp_dict["logo"] = i['shopLogo']
+        temp_dict["info"] = i['content']
+        temp_dict["address"] = address
+        temp_dict["working_hours"] = working_hours
+        final.append(temp_dict)
+
+    with open("Citro.json", "w+", encoding='utf-8') as f:
+        f.write(json.dumps(final, ensure_ascii=False, indent=5))
 
 
 scrap_citro(url)
