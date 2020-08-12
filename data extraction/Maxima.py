523b2ccb2d6bd2069e5ad41350ceccc9023bcf9d
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-12 10:18:55 +0300

    Maxima geojson completed

--- data extraction/Maxima.py
+++ data extraction/Maxima.py
@@ -1,7 +1,8 @@
 import urllib3
 import json
 import re
-from bs4 import BeautifulSoup
+import Geo
+import os
 
 url = "https://www.maxima.lv/ajax/shopsnetwork/map/getCities"
 
@@ -37,3 +38,59 @@ def scrap_maxima(url):
 
 
 scrap_maxima(url)
+
+
+def get_maxima_class(shop):
+    if shop['tag'] == 'abcf790c307391b4214df700051470d2.png':
+        return 'Maxima X'
+    elif shop['tag'] == 'xexpress-4257-2237.png':
+        return 'Maxima Express'
+    elif shop['tag'] == '10b5a0785ed38b910e5677dce6ea894f.png':
+        return 'Maxima XXX'
+    elif shop['tag'] == '63d9d709e7dd5772856dbc8da1b6da62.png':
+        return 'Maxima XX'
+    else:
+        return 'Maxima'
+
+
+def make_geojson(data):
+    most_final = {"type": "FeatureCollection", "features": []}
+    print("Maxima extracting......")
+    # iterate over dictionary
+    for each in data:
+        lat = float(each['lat'].strip()[:17])
+        lon = float(each['lng'].strip()[:17])
+        coordinates = [lon, lat]
+        dms_lat = Geo.fraction_to_min_sec(coordinates[1]) + " N"
+        dms_lon = Geo.fraction_to_min_sec(coordinates[0]) + " E"
+        X = coordinates[0]
+        Y = coordinates[1]
+        X1 = dms_lon
+        Y1 = dms_lat
+        props = dict()
+        additional_props = Geo.get_info(lat, lon)
+        props.update(each)
+        props.update(additional_props)
+        final = dict()
+        props['NOSAUKUMS'] = get_maxima_class(each)
+        props['SUBCATEGORY'] = 'Rimi'
+        props['GRUPA'] = 'Pārtikas/mājsaimniecības preču tīklu veikali'
+        props['X'] = X
+        props['Y'] = Y
+        props['X1'] = X1
+        props['Y1'] = Y1
+        final['type'] = "Feature"
+        final['geometry'] = {"type": "Point", "coordinates": coordinates}
+        final['properties'] = props
+        most_final['features'].append(final)
+
+    return most_final
+
+
+with open('Maxima.json', 'r', encoding='utf8') as f:
+    data = json.loads(f.read())
+    geo_out = make_geojson(data)
+
+    with open(os.path.join(Geo.SAVE_PATH, "Maxima_out.json"), "w+", encoding='utf8') as fayl:
+        fayl.write(json.dumps(geo_out, ensure_ascii=False, indent=6))
+        print("Maxima extracted successfully!")
