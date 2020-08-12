289f0f7c94daa344133d793d7d5132a587f7048b
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-12 11:56:46 +0300

    Citro geojson completed

--- data extraction/Citro.py
+++ data extraction/Citro.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import re
+import os
+import Geo
 from bs4 import BeautifulSoup
 
 url = "https://citro.lv/musu-veikali/"
@@ -61,3 +63,58 @@ def scrap_citro(url):
 
 
 scrap_citro(url)
+
+
+# get store type based on image name (logo-xs | logo-xs-mini)
+def get_citro_type(entry):
+    image_url = entry["logo"]
+    url_parts_list = image_url.split('/')
+    citro_class = url_parts_list[len(url_parts_list) - 1]
+    if citro_class == "logo-xs-mini.svg":
+        return "Citro mini"
+    else:
+        return "Citro"
+
+
+def make_geojson(data):
+    most_final = {"type": "FeatureCollection", "features": []}
+    print("Citro extracting......")
+    # iterate over dictionary
+    for each in data:
+        lat = float(each["coords"]["lat"])
+        lon = float(each["coords"]["lng"])
+        if lat != lon:
+            coordinates = [lon, lat]
+            dms_lat = Geo.fraction_to_min_sec(coordinates[1]) + " N"
+            dms_lon = Geo.fraction_to_min_sec(coordinates[0]) + " E"
+            X = coordinates[0]
+            Y = coordinates[1]
+            X1 = dms_lon
+            Y1 = dms_lat
+            props = dict()
+            additional_props = Geo.get_info(lat, lon)
+            props.update(each)
+            props.update(additional_props)
+            final = dict()
+            props['NOSAUKUMS'] = get_citro_type(each)
+            props['SUBCATEGORY'] = "Citro"
+            props['GRUPA'] = 'Pārtikas/mājsaimniecības preču tīklu veikali'
+            props['X'] = X
+            props['Y'] = Y
+            props['X1'] = X1
+            props['Y1'] = Y1
+            final['type'] = "Feature"
+            final['geometry'] = {"type": "Point", "coordinates": coordinates}
+            final['properties'] = props
+            most_final['features'].append(final)
+
+    return most_final
+
+
+with open('Citro.json', 'r', encoding='utf8') as f:
+    data = json.loads(f.read())
+    geo_out = make_geojson(data)
+
+    with open(os.path.join(Geo.SAVE_PATH, "Citro_out.json"), "w+", encoding='utf8') as fayl:
+        fayl.write(json.dumps(geo_out, ensure_ascii=False, indent=6))
+        print("Citro extracted successfully!")
